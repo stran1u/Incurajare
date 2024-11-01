5ae376055f0320722d63a8cd51f64e9dee98e988
straniu <51492995+stran1u@users.noreply.github.com>
2024-11-01 21:12:13 +0200

    Add files via upload

---
+++ motto.py
@@ -0,0 +1,19 @@
+import random
+
+#lista
+incurajari = [
+    "Ești capabil de lucruri minunate!",
+    "Orice obstacol poate fi depășit!",
+    "Nu uita, fiecare zi e o nouă oportunitate!",
+    "Ai puterea de a-ți transforma visurile în realitate!",
+    "Continuă să fii perseverent, rezultatele vor apărea!",
+    "Ai tot ce-ți trebuie pentru a reuși!",
+    "Încrederea în tine este primul pas către succes!",
+    "Fiecare pas mic te aduce mai aproape de obiectivul tău!",
+    "Ești mai puternic decât crezi!",
+    "Fii blând cu tine însuți și ai încredere în drumul tău!"
+]
+
+#incurajaera
+incurajare_aleasa = random.choice(incurajari)
+print(incurajare_aleasa)
